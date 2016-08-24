9d476b331477e3af94fe753f5bac86c3102bf32c
edward <ecguo@ucdavis.edu>
2016-08-24 04:19:33 -0700

    added tools and stateful whiteboard

--- redraw/src/components/wb.jsx
+++ redraw/src/components/wb.jsx
@@ -5,17 +5,13 @@ class WhiteBoard extends Component {
     static propTypes = {
         width: React.PropTypes.number,
         height: React.PropTypes.number,
-        size: React.PropTypes.number,
         name: React.PropTypes.string,
-        color: React.PropTypes.string
     };
 
     static defaultProps = {
-        width: 500,
-        height: 500,
-        size: 2,
+        width: 400,
+        height: 400,
         name: "canvas",
-        color: "#000"
     };
 
     constructor(props) {
@@ -23,38 +19,53 @@ class WhiteBoard extends Component {
         this.onMouseDown = this.onMouseDown.bind(this);
         this.onMouseMove = this.onMouseMove.bind(this);
         this.onMouseUp = this.onMouseUp.bind(this);
-        this.drawing = false;
+        this.clear = this.clear.bind(this);
+        this.undo = this.undo.bind(this);
+        this.redo = this.redo.bind(this);
+        this.state = {drawing: false, undo: false, color: "#000000", size: 2}
         this.points = [];
         this.hist = [];
+        this.redo_hist = [];
+    }
+
+    setDrawing() {
+        this.setState({drawing: !this.state.drawing});
+    }
+    setColor(c) {
+        this.setState({color: c});
+    }
+    setSize(s) {
+        this.setState({size: s});
     }
 
     componentDidMount() {
-        this.canvas = findDOMNode(this);
+        this.canvas = this.refs.wb;
         this.ctx = this.canvas.getContext('2d');
     }
 
     onMouseDown(e) {
         var pos = this.xy(e);
-        this.ctx.strokeStyle = this.props.color;
-        this.ctx.lineWidth = this.props.size;
+        this.setState({undo: false});
+        this.ctx.strokeStyle = this.state.color;
+        this.ctx.lineWidth = this.state.size;
         this.points.push({
             pos: pos,
-            color: this.props.color,
-            size: this.props.size 
+            color: this.state.color,
+            size: this.state.size 
         });
         this.ctx.beginPath();
         this.ctx.moveTo(pos.x, pos.y);
-        this.drawing = true;
+        this.setDrawing();
     }
 
 
     onMouseMove(e) {
-        if (this.drawing) {
+        if (this.state.drawing) {
             var pos = this.xy(e);
             this.points.push({
                 pos: pos,
-                color: this.props.color,
-                size: this.props.size 
+                color: this.state.color,
+                size: this.state.size 
             });
             this.ctx.lineTo(pos.x, pos.y);
             this.ctx.stroke();
@@ -63,9 +74,9 @@ class WhiteBoard extends Component {
 
 
     onMouseUp(e) {
-        if (this.drawing) {
+        if (this.state.drawing) {
             this.onMouseMove(e);
-            this.drawing = false;
+            this.setDrawing();
             this.hist.push(this.points);
             this.points = []
         }
@@ -79,9 +90,53 @@ class WhiteBoard extends Component {
         }
     };
 
+    clear() {
+        this.ctx.clearRect(0,0,this.props.width, this.props.height);
+        this.points=[];
+    }
+
+    undo() {
+        if (this.hist.length > 0) {
+            this.clear();
+            this.redo_hist.push(this.hist[this.hist.length - 1]);
+            this.hist.pop()
+            this.redraw();
+            this.points = [];
+            this.setState({undo: true});
+            console.log(this.state.undo);
+        }
+    }
+
+    redo() {
+        if (this.redo_hist.length > 0 && this.state.undo == true) {
+            this.clear();
+            this.hist.push(this.redo_hist[this.redo_hist.length - 1]);
+            this.redo_hist.pop()
+            this.redraw();
+            this.points = [];
+        }
+    }
+
+    redraw() {
+        for (var i = 0; i < this.hist.length; i++) {
+            this.points = this.hist[i];
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.points[0].pos.x, this.points[0].pos.y);
+            this.ctx.strokeStyle = this.points[0].color;
+            this.ctx.lineWidth = this.points[0].size;
+            for (var j = 0; j < this.points.length; j++) {
+                this.ctx.strokeStyle = this.points[j].color;
+                this.ctx.lineWidth = this.points[j].size;
+                this.ctx.lineTo(this.points[j].pos.x, this.points[j].pos.y);
+            }
+            this.ctx.stroke();
+        }
+    }
+
     render() {
         const {width, height, name} = this.props;
         return (
+            <div>
             <canvas 
             onMouseDown={this.onMouseDown}
             onMouseMove={this.onMouseMove}
@@ -90,7 +145,28 @@ class WhiteBoard extends Component {
             width={width}
             height={height}
             className={name}
+            ref="wb"
             />
+            <div className="options" style={{marginBottom:20}}>
+            <label htmlFor="">size: </label>
+            <input min="1" max="20" type="range" value={this.state.size} onChange={(e) => this.setSize(parseInt(e.target.value))} />
+            </div>
+            <div className="options" style={{marginBottom:20}}>
+            <label htmlFor="">color: </label>
+            <input type="color" value={this.state.color} onChange={(e) => this.setColor(e.target.value)} />
+            </div>
+            <div className="tools" style={{marginBottom:20}}>
+            <button className={'clear'} onClick={this.clear}>
+            clear
+            </button>
+            <button className={'undo'} onClick={this.undo}>
+            undo
+            </button>
+            <button className={'redo'} onClick={this.redo}>
+            redo
+            </button>
+            </div>
+            </div>
         )
     }
 }
