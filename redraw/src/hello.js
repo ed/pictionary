a27753a30e248fc0020bf0d38dbe6f8c35d72e89
edward <ecguo@ucdavis.edu>
2016-08-24 02:02:35 -0700

    got basic whiteboard rewritten in react

--- redraw/src/hello.js
+++
@@ -1,4 +0,0 @@
-module.exports = function() {
-    var hello = console.log("hello world");
-    return hello;
-}
--- redraw/webpack.config.js
+++ redraw/webpack.config.js
@@ -2,14 +2,18 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: __dirname + '/src/main.js',
+    entry: __dirname + '/src/main.jsx',
     output: {
         path: __dirname + '/bin',
         filename: 'bundle.js'
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     module: {
         loaders:[
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel' },
             { test: /\.css$/, loader: 'style!css' }
         ]
     },
