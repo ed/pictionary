43fa9fae8fa0f9d49aceef22159291510a7400e5
edward <ecguo@ucdavis.edu>
2016-08-26 01:23:46 -0700

    readded unpackMessage function

--- src/utils/MessageUtils.js
+++ src/utils/MessageUtils.js
@@ -3,6 +3,15 @@ import uuid from 'node-uuid';
 var socket = io.connect();
 
 module.exports = {
+    unpackMessage: function(message, currentThreadID) {
+        return {
+            id: message.id,
+            threadID: message.threadID,
+            authorName: message.authorName,
+            text: message.text,
+            timestamp: (new Date).toISOString().replace(/z|t/gi,' ').trim()
+        };
+    },
     createMessage: function(text, author, currentThreadID) {
         var message = {
             id: 'm_'+uuid.v4(),
@@ -11,7 +20,7 @@ module.exports = {
             text: text,
             timestamp: (new Date).toISOString().replace(/z|t/gi,' ').trim()
         };
+        // redis store here
         socket.emit('chat msg', message);
     }
-    // redis store here
-}
+};
