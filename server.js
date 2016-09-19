8c680784bde2ab63e8b1f70fafaa7cc2f8ba5a3d
Eugene Fairley <ecfairley@ucdavis.edu>
2016-09-18 17:41:22 -0700

    for production

--- server.js
+++ server.js
@@ -99,20 +99,20 @@ function endGame() {
    *   Run 'webpack --config webpack.dev.config' for dev mode
    */
 
-if (process.env.NODE_ENV !== 'production') {
-  var config = require('./webpack.dev.config');
-  var webpack = require('webpack');
-  var WebpackDevServer = require('webpack-dev-server');
-  new WebpackDevServer(webpack(config), {
-    hot: true,
-    colors: true,
-    inline: true,
-    proxy: {'**': 'http://localhost:3000'},
-    historyApiFallback: true
-  }).listen(3001, 'localhost', function (err, result) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('Dev Server listening at http://localhost:3001/');
-  });
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   var config = require('./webpack.dev.config');
+//   var webpack = require('webpack');
+//   var WebpackDevServer = require('webpack-dev-server');
+//   new WebpackDevServer(webpack(config), {
+//     hot: true,
+//     colors: true,
+//     inline: true,
+//     proxy: {'**': 'http://localhost:3000'},
+//     historyApiFallback: true
+//   }).listen(3001, 'localhost', function (err, result) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log('Dev Server listening at http://localhost:3001/');
+//   });
+// }
