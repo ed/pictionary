8438ab24243aec4a614038a6739aa88acf16ef20
Eugene Fairley <ecfairley@ucdavis.edu>
2016-10-14 17:34:01 -0700

    login, register in modals

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,20 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import { Link } from 'react-router'
+import Popover from './Popover'
+import Register from './Register'
+import Login from './Login'
 
 class Home extends Component {
+
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	loggingIn: false,
+	  	signingUp: false
+	  };
+	}
 
 	componentDidMount() {
     if (this.props.authStatus) {
@@ -11,19 +23,36 @@ class Home extends Component {
     }
   }
 
+  openLogin() { this.setState({ loggingIn: true }); }
+
+  closeLogin() { this.setState({ loggingIn: false }); }
+
+  openSignup() { this.setState({ signingUp: true }); }
+
+  closeSignup() { this.setState({ signingUp: false }); }
+
 	render() {
 		return (
 			<div className="container" style={{position:'absolute', top: 0, left: 0, right:0, bottom: 0, opacity: '.9', background: 'url(../../img/background.jpg) no-repeat center center fixed', backgroundSize: 'cover'}}>
 			<div className="popoverContainer" style={{color: 'white', textAlign: 'center'}}>
-				<button className="myButton active" style={{marginBottom: '20px', position: 'relative'}} onClick={() => this.props.dispatch(push('/register'))}>Sign Up</button>
+				<button className="myButton active" style={{marginBottom: '20px', position: 'relative'}} onClick={() => this.openSignup()}>Sign Up</button>
 				<br/>
 				or
 				<br/>
 				<button className="myButton active" style={{marginTop: '20px', position: 'relative'}} onClick={() => this.props.dispatch(push('/game'))}>Play</button>
 			</div>
 			<ul style={{position: 'absolute', left: 0, top: 0, width: '100%'}}>
-			  <li><a style={{}} onClick={() => this.props.dispatch(push('/login'))}>Log in</a></li>
+			  <li><a style={{}} onClick={() => this.openLogin()}>Log in</a></li>
 			</ul>
+
+			<Popover close={() => this.closeSignup()} isOpen={this.state.signingUp}>
+        <Register close={() => this.closeSignup()}/> 
+      </Popover>
+
+      <Popover close={() => this.closeLogin()} isOpen={this.state.loggingIn}>
+        <Login close={() => this.closeLogin()} /> 
+      </Popover>
+
 			</div>
 		)
 	}
